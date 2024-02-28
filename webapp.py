d89df83543c8b66f449c5f8ef6f980f056cb0a53
axelToussenel <axel.toussenel@gmail.com>
2024-02-28 16:26:27 +0100

    insertion mongodb

--- webapp.py
+++ webapp.py
@@ -1,21 +1,37 @@
 import json
 import requests
 from pymongo import MongoClient
+import pymongo
 import sys
-from .utils.scrapUtils import ScrapUtils
+from utils.__init__ import __init__
+from utils.scrapUtils import ScrapUtils
 
 
 
+def get_database():
+    MONGO_CONNEXION = "mongodb+srv://axel_toussenel:AxelAdmin92@car-thesiendb.sey3qsk.mongodb.net/"
+    client = MongoClient(MONGO_CONNEXION)
+    return(client)
 
 
-scrap = ScrapUtils('https://www.aramisauto.com/achat/occasion?types%5B0%5D=VO')
+#EXECUTION GLOBALE
+URL = 'https://www.aramisauto.com/achat/occasion?types%5B0%5D=VO'
+scrap = ScrapUtils(URL)
 
-client = MongoClient("localhost", 27017, "scrap_2024")
-collection = client.Projet_Ydays.scrap_2024
+print("-----------------------------MONGO CONNEXION-----------------------------")
+client=get_database()
+database = client["Projet_Ydays"]
+collection = database["scrap_2024"]
 
-scrap.main_scrap()
-
-collection.insert_many('exports/car_data.json')
-
-count = collection.count_documents({})
-print('Nombre de documents en base : '+count)
+print("------------------------SCRAPPING & BASE INSERTION-----------------------")
+#scrap.main_scrap()
+try:
+    with open('exports/car_data.json', 'r') as jsonfile :
+        car_data = json.load(jsonfile)
+    collection.insert_many(car_data)
+    print('Insertion effectuée')
+    count = collection.count_documents({})
+    print('Nombre de documents en base:')
+    print(count)
+except pymongo.errors.ConnectionFailure as e:
+    print('Insertion échouée.  --> ', e)

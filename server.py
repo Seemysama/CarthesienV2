4344f4b8022f654533c201f7024aeeae57794723
axelToussenel <axel.toussenel@gmail.com>
2024-03-27 22:35:37 +0100

    server + orga code

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
 
@@ -14,6 +14,26 @@ collection = db['scrap_2024']
 def get_data():
     data = list(collection.find({}, {'_id': False}))  # Replace 'data' with your collection name
     return jsonify(data)
+
+@app.route("/api/formulaire", methods=["POST"])
+def formulaire():
+    data = request.get_json()
+    marque = data["marque"]
+    modele = data["modele"]
+    prix = data["prix"]
+    motorisation = data["motorisation"]
+    carburant = data["carburant"]
+    annee = data["annee"]
+    kms = data["kms"]
+    options = data["options"]
+    print(marque, modele, prix, motorisation, carburant, annee, kms, options)
+    return {"success": True}
+
+@app.route("/api/classification", methods=["POST"])
+def classification():
+    data = request.get_json()
+    classification = 0
+    return classification
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3030)
---
+++ site.jsx
@@ -0,0 +1,11 @@
+import Formulaire from "./formulaire";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Formulaire />
+  </React.StrictMode>,
+)
+
+
